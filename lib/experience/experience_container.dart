2a5a2d83629a6069dac3cc4b2a4ea8576c425954
Silvano <silvano.norberti@gmail.com>
2022-02-27 00:09:52 +0100

    Updated experiences and company's name are links

--- lib/experience/experience_container.dart
+++ lib/experience/experience_container.dart
@@ -1,6 +1,9 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio_flutter_web/utils/colour_assets.dart';
+import 'package:url_launcher/link.dart';
 
 class ExperienceContainer extends StatelessWidget {
   const ExperienceContainer({
@@ -26,10 +29,10 @@ class ExperienceContainer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            experience.company,
-            style: textStyle(isBold: true),
-          ),
+          new InkWell(
+              child: new Text(experience.company.getTitle),
+              onTap: () =>
+                  html.window.open(experience.company.getLink, "_blank")),
           SizedBox(height: 10),
           Text(
             experience.timeLine,
@@ -55,8 +58,23 @@ TextStyle textStyle({bool isBold, bool isGrey}) {
       color: isGrey ?? false ? Colors.grey : Colors.black);
 }
 
+class CompanyLink {
+  final String link;
+  final String title;
+
+  CompanyLink(this.link, this.title);
+
+  String get getLink {
+    return link;
+  }
+
+  String get getTitle {
+    return title;
+  }
+}
+
 class ExperienceInfo {
-  final String company;
+  final CompanyLink company;
   final String timeLine;
   final List<String> descriptions;
 
@@ -70,24 +88,40 @@ class ExperienceInfo {
 // I didn't use multi-line because it cause a weird render of the description.
 final experiences = [
   ExperienceInfo(
-    company: 'Welld',
-    timeLine: 'May 2019 - Now',
+    company: new CompanyLink('https://www.dstech.it/', 'Ds Tech '),
+    timeLine: 'Nov 2021 - Now',
     descriptions: [
-      "Nel tempo trascorso nell’azienda ho avuto occasioni di aumentare le mie " +
-          "conoscenze in varie tecnologie. Tra le più utilizzate ci sono React, " +
-          "Typescript e Java (JEE). Oltre agli sviluppi sia lato BackEnd che FrontEnd " +
-          "ho avuto occasione di sviluppare immagini Docker per la distribuzione degli " +
-          "applicativi, per facilitare l’utilizzo delle applicazioni esistenti a chi " +
-          "non avesse un ambiente pronto da utilizzare e per attuare test di integrazione. " +
-          "Intensivo uso di Electron insieme a React e Redux, tutto scritto in Typescript. " +
-          "Utilizzo di TSDX per la compilazione e la distribuzione della nostra libreria agli applicativi associati. " +
-          "Scritto test di integrazione tramite Cypress. " +
-          "Continua scrittura di test unitari per ogni parte di codice aggiunta o già esistente. Utilizzando sia Jest sia Junit. " +
-          "Quotidiano utilizzo di Git"
+      """
+        Ho lavorato ad un progetto di Enel come unico sviluppatore front end. 
+        Il progetto in questione è un software dove il cliente si occupa di compilare la documentazione (una sorta di form). 
+        In autonomia gestivo il progetto e implementavo tutte le sistemazioni e nuovi sviluppi che richiedeva il cliente.
+        Avendo avuto piena libertà sul progetto ho avuto il bisogno di attuare un refactoring 
+        regolare del codice ogni qual volta la sezione su cui lavoravo ne aveva bisogno (quasi ogni sezione), 
+        introdotto migliorie di performance, come un miglior utilizzo degli hooks, e stilizzazioni responsive. 
+      """
     ],
   ),
   ExperienceInfo(
-    company: 'Edr TOOLS',
+    company: new CompanyLink('https://www.welld.ch/', 'Welld'),
+    timeLine: 'May 2019 - Nov 2021',
+    descriptions: [
+      """
+          Nel tempo trascorso nell’azienda ho avuto occasioni di aumentare le mie 
+          conoscenze in varie tecnologie. Tra le più utilizzate ci sono React, 
+          Typescript e Java (JEE). Oltre agli sviluppi sia lato BackEnd che FrontEnd 
+          ho avuto occasione di sviluppare immagini Docker per la distribuzione degli 
+          applicativi, per facilitare l’utilizzo delle applicazioni esistenti a chi 
+          non avesse un ambiente pronto da utilizzare e per attuare test di integrazione. 
+          Intensivo uso di Electron insieme a React e Redux, tutto scritto in Typescript. 
+          Utilizzo di TSDX per la compilazione e la distribuzione della nostra libreria agli applicativi associati. 
+          Scritto test di integrazione tramite Cypress. 
+          Continua scrittura di test unitari per ogni parte di codice aggiunta o già esistente. Utilizzando sia Jest sia Junit. 
+          Quotidiano utilizzo di Git
+      """
+    ],
+  ),
+  ExperienceInfo(
+    company: new CompanyLink('https://edrtools.eu/', 'EDR Tools'),
     timeLine: 'Sep 2018 - Apr 2019',
     descriptions: [
       'Estrazione dei dati da dispositivi mobili con software creati per Raspberry con l\'utilizzo di librerie in C e Python. ' +
@@ -96,7 +130,8 @@ final experiences = [
     ],
   ),
   ExperienceInfo(
-    company: 'ITS Kennedy',
+    company: new CompanyLink(
+        'https://www.tecnicosuperiorekennedy.it/', 'ITS Alto Adriatico'),
     timeLine: '2016 - 2018',
     descriptions: [
       'Attività e associazioni: Studio e praticità nel corso di studi I.O.T (Internet of Things) \n' +
